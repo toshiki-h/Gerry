0858e8d87a46324c181cced37807b2259863eb02
Jonathan Frieß <jonathan.friess@fau.de>
2018-07-11 18:15:04 +0200

    add first tests for get_changes()

--- test_gerry.py
+++ test_gerry.py
@@ -4,7 +4,6 @@ import gerry
 
 
 class DatetimeToString(unittest.TestCase):
-
     def test_datetime_to_string(self):
         date = datetime.datetime(2018, 1, 1)
         self.assertEqual(gerry.datetime_to_string(
@@ -16,7 +15,6 @@ class DatetimeToString(unittest.TestCase):
 
 
 class CreateTimeFrames(unittest.TestCase):
-
     def test_create_time_frames_year(self):
         start_date = datetime.datetime(2017, 1, 1)
         end_date = datetime.datetime(2018, 1, 1)
@@ -30,6 +28,21 @@ class CreateTimeFrames(unittest.TestCase):
         timeframes = gerry.create_time_frames(
             start_date, end_date, datetime.timedelta(hours=1))
         self.assertEqual(len(timeframes), 24)
+
+
+class GerryCrawler(unittest.TestCase):
+    def setUp(self):
+        self.gerry = gerry.GerryCrawler('gerrit', 'https://gerrit-review.googlesource.com',
+                                        datetime.datetime(2018, 6, 1), datetime.datetime(2018, 6, 2), './gerry_data/')
+
+    def test_get_changes(self):
+        changes = self.gerry.get_changes(datetime.datetime.strptime('2018-06-01', '%Y-%m-%d'))
+        self.assertEqual(len(changes), 21)
+        self.assertEqual(changes[0]['change_id'], 'Ib051dd347eaea2c77ae6c403ebf76bed4b9b4b9c')
+
+    def test_get_changes_no_data(self):
+        changes = self.gerry.get_changes(datetime.datetime.strptime('5018-06-01', '%Y-%m-%d'))
+        self.assertEqual(len(changes), 0)
 
 
 if __name__ == '__main__':
